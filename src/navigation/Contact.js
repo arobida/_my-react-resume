0485d5b87fc12497c9e91df6dd0d0544672395b1
Andrew Robida <afrobida@me.com>
2018-07-14 07:34:21 -0700

    :bug:

--- src/navigation/Contact.js
+++ src/navigation/Contact.js
@@ -26,7 +26,7 @@ class Contact extends Component {
   handleChange = e => this.setState({ [e.target.name]: e.target.value });
 
   handleSubmit = e => {
-    axios("https://arfolio.netlify.com/contact", {
+    axios("/", {
       method: "POST",
       headers: { "Content-Type": "application/x-www-form-urlencoded" },
       body: encode({ "form-name": "contact", ...this.state })
